dad6c1a736a2ea65ce1f8ee68db49d7d1cd33aeb
Renan <rmavansi@gmail.com>
2019-12-15 16:44:59 -0300

    initial commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+import Background from '~/components/Background';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Text>SignIn</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Type your name"
+      />
+      <Button>Login</Button>
+    </Background>
+  );
+}
